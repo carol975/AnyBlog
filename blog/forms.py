114fa0fbf56f0c42113a1298cf8a5471f8476e05
carol975 <carol975@users.noreply.github.com>
2020-06-23 20:02:15 -0700

    add function to update user account infor\

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
@@ -32,3 +34,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])    
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    # TODO add more file support`
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update Account')
+
+    # Custom WTF validators: https://stackoverflow.com/questions/50327174/custom-validators-in-wtforms-using-flask
+    # only check if account info is changed
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('this username already exists, please choose another one')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('this email already exists, please choose another one')
--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,13 @@
+import secrets
+import os
+
 from blog import app, db, bcrypt
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blog.models import User, Post
 
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, logout_user, current_user, login_required
+from PIL import Image
 from sqlalchemy.exc import SQLAlchemyError
 
 
@@ -85,7 +89,51 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static', 'profile_pics', picture_fn)
+    
+    #resize image to 125x125 pixel
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html',title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            # TODO use external picture server
+            # TODO remove previous user picture if exist
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        # do not return a rendered form, it will give a form resubmit error
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    # sessions :https://docs.sqlalchemy.org/en/13/orm/session_basics.html#session-faq-whentocreate
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html',title='Account', image_file=image_file, form=form)
+
+# TODO use blueprint to separate the routes
+@app.route('/post/new')
+@login_required
+def new_post():
+    # flask login knows which user is logged in
+    logout_user()
+    return render_template('create_post.html', title='New Post')
