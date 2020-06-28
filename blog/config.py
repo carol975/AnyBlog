337dfda1d78ff1c9f437783802b668454a4e9636
carol975 <carol975@users.noreply.github.com>
2020-06-28 16:06:33 -0700

    remove db file

--- blog/config.py
+++ blog/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') #use python secret module
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' #os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
