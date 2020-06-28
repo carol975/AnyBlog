17321157434a4aef57e730e8ded25fb34cd7707b
carol975 <carol975@users.noreply.github.com>
2020-06-28 16:10:52 -0700

    add sqlite

--- blog/__init__.py
+++ blog/__init__.py
@@ -33,12 +33,9 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-
 
     from blog.users.routes import users
     from blog.posts.routes import posts
